11d56c0a8336943643ec1a6f8f58e64ddbd5ef5f
Timothy Merritt <timmybytes@gmail.com>
2021-05-09 07:46:21 -0700

    chore(Nav): Add mobile menu trigger

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,11 +1,67 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import Link from 'next/link';
-import Image from 'next/image';
 import AuthContext from '../stores/authContext';
 import styles from '../styles/Navbar.module.scss';
 
 export default function Navbar() {
   const { user, login, logout, authReady } = useContext(AuthContext);
+  const [screenWidth, setScreenWidth] = useState(null);
+  const [burger, setBurger] = useState(false);
+
+  const burgerMenu = () => {
+    return (
+      <>
+        <li>
+          <Link href='/'>
+            <a>Home</a>
+          </Link>
+        </li>
+        <li>
+          <Link href='/about'>
+            <a>About</a>
+          </Link>
+        </li>
+        <li>
+          <Link href='/store'>
+            <a>Store</a>
+          </Link>
+        </li>
+        <li>
+          <Link href='/members'>
+            <a>Members Only</a>
+          </Link>
+        </li>
+        {authReady && (
+          <>
+            {!user && (
+              <li className={styles.btn} onClick={login}>
+                Login/Signup
+              </li>
+            )}
+            {user && (
+              <li>
+                <strong>{user.user_metadata.full_name}</strong>
+              </li>
+            )}
+            {user && (
+              <li className={styles.btn} onClick={logout}>
+                Logout
+              </li>
+            )}
+          </>
+        )}
+      </>
+    );
+  };
+
+  useEffect(() => {
+    function handleResize() {
+      console.log('resized to: ', window.innerWidth, 'x', window.innerHeight);
+      setScreenWidth(window.innerWidth);
+    }
+    window.addEventListener('resize', handleResize);
+    screenWidth <= 640 ? setBurger(true) : setBurger(false);
+  });
 
   return (
     <div className={styles.container}>
@@ -19,8 +75,10 @@ export default function Navbar() {
             />
           </a>
         </Link>
-        <ul>
-          <li>
+        {/* NOTE: Comment out and reload site during development for authReady-dependent code to load, then enter the Netlify site URL into the login modal.  */}
+        <ul style={burger ? { display: 'none' } : { display: 'initial' }}>
+          {burgerMenu}
+          {/* <li>
             <Link href='/'>
               <a>Home</a>
             </Link>
@@ -40,7 +98,6 @@ export default function Navbar() {
               <a>Members Only</a>
             </Link>
           </li>
-          {/* NOTE: Comment out and reload site during development for authReady-dependent code to load, then enter the Netlify site URL into the login modal.  */}
           {authReady && (
             <>
               {!user && (
@@ -59,11 +116,15 @@ export default function Navbar() {
                 </li>
               )}
             </>
-          )}
+          )} */}
         </ul>
-        {/* <div className='banner'>
-        <Image src='/banner.png' width={966} height={276} />
-      </div> */}
+        <button
+          className={styles.burger}
+          style={burger ? { display: 'initial' } : { display: 'none' }}
+          onClick={() => alert('clicked')}>
+          {screenWidth}
+        </button>
+        <div className={styles.menu}>{burgerMenu()}</div>
       </nav>
     </div>
   );
