e8bba027639af25376028d8eefb8fae7ca460890
Timothy Merritt <timmybytes@gmail.com>
2021-05-25 11:08:13 -0700

    chore: Add album data

---
+++ albums.js
@@ -0,0 +1,75 @@
+export const simulacra = [
+  {
+    num: 1,
+    name: 'Quagmire',
+  },
+  {
+    num: 2,
+    name: 'Tabs On Your Insides',
+  },
+  {
+    num: 3,
+    name: 'In Your Crime',
+  },
+  {
+    num: 4,
+    name: 'Mammoth & the Rage',
+  },
+  {
+    num: 5,
+    name: 'Thumper',
+  },
+  {
+    num: 6,
+    name: 'Nova Lady Ache',
+  },
+  {
+    num: 7,
+    name: 'And Come Undone',
+  },
+  {
+    num: 8,
+    name: 'Trepidation Highway',
+  },
+  {
+    num: 9,
+    name: 'Simulacrum I',
+  },
+  {
+    num: 10,
+    name: 'Southsea Hants',
+  },
+  {
+    num: 11,
+    name: 'Wear Their Hunger & Their Haunt',
+  },
+];
+
+export const multnomah = [
+  { num: 1, name: 'Terra Firma' },
+  { num: 2, name: 'Profanity' },
+  { num: 3, name: 'Drown the Pariah' },
+  { num: 4, name: "Wolf in Sheep's Clothing" },
+  { num: 5, name: 'Sway' },
+  { num: 6, name: 'Cascadia' },
+  { num: 7, name: 'An Evening Trip' },
+  { num: 8, name: 'Ballad For a Figment' },
+  { num: 9, name: 'Rattle Me' },
+  { num: 10, name: 'A Melody of Old' },
+];
+
+export const faitAccompli = [
+  { num: 1, name: 'Snooze' },
+  { num: 2, name: 'Write the Wrongs' },
+  { num: 3, name: 'A Quiet Cult' },
+  { num: 4, name: 'Strayaway' },
+  { num: 5, name: 'Sloth & Decay' },
+  { num: 6, name: 'Chimera' },
+  { num: 7, name: 'Pitter Patter' },
+  { num: 8, name: 'Go Alight' },
+  { num: 9, name: 'Warhorse' },
+  { num: 10, name: 'Big as Life' },
+  { num: 11, name: 'String Me Up' },
+  { num: 12, name: 'Out in Rays' },
+  { num: 13, name: 'Systems, Mobs, and Bob' },
+];
