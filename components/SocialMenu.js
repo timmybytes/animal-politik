d4fe4fa3d5414fd9b2c90d66943233e6e851fc24
Timothy Merritt <timmybytes@gmail.com>
2021-05-31 07:47:07 -0700

    chore: Add noreferrer to outbound links

--- components/SocialMenu.js
+++ components/SocialMenu.js
@@ -18,46 +18,64 @@ const SocialMenu = () => {
       <motion.div className={styles.social__container}>
         <a
           href='https://music.apple.com/us/artist/animal-politik/1494225714'
-          target='_blank'>
+          target='_blank'
+          rel='noreferrer nofollow'>
           <SiApplemusic />
         </a>
       </motion.div>
       <motion.div className={styles.social__container}>
         <a
           href='https://open.spotify.com/artist/72RvQ5IlKW2zrVlPuxSr9h'
-          target='_blank'>
+          target='_blank'
+          rel='noreferrer nofollow'>
           <SiSpotify />
         </a>
       </motion.div>
       <motion.div className={styles.social__container}>
-        <a href='https://animalpolitik.bandcamp.com/' target='_blank'>
+        <a
+          href='https://animalpolitik.bandcamp.com/'
+          target='_blank'
+          rel='noreferrer nofollow'>
           <SiBandcamp />
         </a>
       </motion.div>
       <motion.div className={styles.social__container}>
-        <a href='https://soundcloud.com/user-511973193' target='_blank'>
+        <a
+          href='https://soundcloud.com/user-511973193'
+          target='_blank'
+          rel='noreferrer nofollow'>
           <SiSoundcloud />
         </a>
       </motion.div>
       <motion.div className={styles.social__container}>
-        <a href='https://twitter.com/nimalpolitik' target='_blank'>
+        <a
+          href='https://twitter.com/nimalpolitik'
+          target='_blank'
+          rel='noreferrer nofollow'>
           <SiTwitter />
         </a>
       </motion.div>
       <motion.div className={styles.social__container}>
         <a
           href='https://music.youtube.com/channel/UCKg5BbKgpP3820f05b6JYbg'
-          target='_blank'>
+          target='_blank'
+          rel='noreferrer nofollow'>
           <SiYoutube />
         </a>
       </motion.div>
       <motion.div className={styles.social__container}>
-        <a href='https://www.instagram.com/animal.politik/' target='_blank'>
+        <a
+          href='https://www.instagram.com/animal.politik/'
+          target='_blank'
+          rel='noreferrer nofollow'>
           <SiInstagram />
         </a>
       </motion.div>
       <motion.div className={styles.social__container}>
-        <a href='https://ko-fi.com/animalpolitik' target='_blank'>
+        <a
+          href='https://ko-fi.com/animalpolitik'
+          target='_blank'
+          rel='noreferrer nofollow'>
           <SiKoFi />
         </a>
       </motion.div>
