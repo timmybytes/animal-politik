3f45435444160c3bc82d1fa0f45a6b2e9d1f2e7e
Timothy Merritt <timmybytes@gmail.com>
2021-06-08 07:10:48 -0700

    build(SCSS): Add support for SCSS paths/globals

--- next.config.js
+++ next.config.js
@@ -1,1 +1,9 @@
-module.exports = { future: { webpack5: false }, target: 'serverless' };
+const path = require('path');
+
+module.exports = {
+  future: { webpack5: false },
+  target: 'serverless',
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+};
