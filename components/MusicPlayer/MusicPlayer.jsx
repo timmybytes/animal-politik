d6c5587e02a427f8ff21b6ff31fd6a38c888f5d0
Timothy Merritt <timmybytes@gmail.com>
2021-06-10 13:28:01 -0700

    feat: Add mapped audio player --WIP

--- components/MusicPlayer/MusicPlayer.jsx
+++ components/MusicPlayer/MusicPlayer.jsx
@@ -1,38 +1,66 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from '@components/MusicPlayer/MusicPlayer.module.scss';
+import { simulacra } from '../../musicData.js';
 
-// <player>
-//   <trackInfo>
-//     <art></art>
-//     <album></album>
-//     <year></year>
-//     <share></share>
-//     <download></download>
-//   </trackInfo >
-//   <audio></audio>
-// </player >
+const trackPlayer = ({ track, children, ...props }) => {
+  return (
+    <div>
+      <img src={track.artwork} alt={`Artwork for ${track.album}`} />
+      <p>{track.title}</p>
+      <p>{track.album}</p>
+      <audio src={track.audio}></audio>
+    </div>
+  );
+};
 
 const MusicPlayer = () => {
+  const [currentSong, setCurrentSong] = useState();
+
+  const handleSinglePlay = e => {
+    // console.log(e.target.title);
+    console.log(e);
+    setCurrentSong(e.target.title);
+  };
+
+  const handlePlay = e => {
+    console.log('play');
+  };
+
+  const handleStop = e => {
+    console.log('stop');
+  };
   return (
-    <>
-      <div className={styles.player}>
-        <div className={styles.trackInfo}>
-          <h3>Quagmire</h3>
-          <h4>
-            Simulacra <span>•</span>
-            <span className={styles.year}>2021</span>
-          </h4>
-        </div>
-        <audio autoPlay={false} controls>
-          <source src='/songs/Quagmire.mp3' type='audio/mp3' />
-          <p>
-            Your browser does not support the
-            <code>audio</code> element. Here is a{' '}
-            <a href='/songs/Quagmire.mp3'>link to the audio</a> instead.
-          </p>
-        </audio>
-      </div>
-    </>
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'stretch',
+        justifyItems: 'stretch',
+        alignContent: 'stretch',
+        width: '100%',
+      }}
+      className={styles.player}>
+      {simulacra.map(song => {
+        return (
+          <div style={{ width: '100%' }}>
+            <p>{song.title}</p>
+            <audio
+              onPlay={handlePlay}
+              onPause={handleStop}
+              src={song.audio}
+              controls
+              playsInline={true}
+              loop={false}
+              autoPlay={false}
+              title={song.title}
+              type={currentSong === song.title ? 'play' : 'pause'}>
+              Your browser does not support HTML5 audio! But you can still
+              download this song here: <a href={song.audio}>{song.title}</a>
+            </audio>
+          </div>
+        );
+      })}
+    </div>
   );
 };
 
---
+++ musicData.js
@@ -0,0 +1,283 @@
+const HOME = 'https://animalpolitik.netlify.app';
+const ARTWORK = '/album-covers';
+const AUDIO = '/songs';
+
+export const songs = {};
+
+export const music = [
+  {
+    simulacra: {
+      artwork: `${ARTWORK}/simulacra-cover.jpg`,
+      tracks: [
+        {
+          audio: `${AUDIO}/simulacra/quagmire.mp3`,
+          title: 'Quagmire',
+          trackNumber: 1,
+        },
+        {
+          audio: `${AUDIO}/simulacra/tabs-on-your-insides.mp3`,
+          title: 'Tabs On Your Insides',
+          trackNumber: 2,
+        },
+        {
+          audio: `${AUDIO}/simulacra/in-your-crime.mp3`,
+          title: 'In Your Crime',
+          trackNumber: 3,
+        },
+        {
+          audio: `${AUDIO}/simulacra/mammoth-and-the-rage.mp3`,
+          title: 'Mammoth & the Rage',
+          trackNumber: 4,
+        },
+        {
+          audio: `${AUDIO}/simulacra/thumper.mp3`,
+          title: 'Thumper',
+          trackNumber: 5,
+        },
+        {
+          audio: `${AUDIO}/simulacra/nova-lady-ache.mp3`,
+          title: 'Nova Lady Ache',
+          trackNumber: 6,
+        },
+        {
+          audio: `${AUDIO}/simulacra/and-come-undone.mp3`,
+          title: 'And Come Undone',
+          trackNumber: 7,
+        },
+        {
+          audio: `${AUDIO}/simulacra/trepidation-highway.mp3`,
+          title: 'Trepidation Highway',
+          trackNumber: 8,
+        },
+        {
+          audio: `${AUDIO}/songs/simulacrum-i.mp3`,
+          title: 'Simulacrum I',
+          trackNumber: 9,
+        },
+        {
+          audio: `${AUDIO}/simulacra/southsea-hants.mp3`,
+          title: 'Southsea Hants',
+          trackNumber: 10,
+        },
+        {
+          audio: `${AUDIO}/simulacra/wear-their-hunger-and-their-haunt.mp3`,
+          title: 'Wear Their Hunger & Their Haunt',
+          trackNumber: 11,
+        },
+      ],
+    },
+  },
+];
+
+// export const multnomah = [
+//   {
+//     artwork: '',
+//     audio: '',
+//     title: 'Terra Firma',
+//     trackNumber: 1,
+//   },
+//   {
+//     artwork: '',
+//     audio: '',
+//     title: 'Profanity',
+//     trackNumber: 2,
+//   },
+//   {
+//     artwork: '',
+//     audio: '',
+//     title: 'Drown the Pariah',
+//     trackNumber: 3,
+//   },
+//   {
+//     artwork: '',
+//     audio: '',
+//     title: "Wolf in Sheep's Clothing",
+//     trackNumber: 4,
+//   },
+//   {
+//     artwork: '',
+//     audio: '',
+//     title: 'Sway',
+//     trackNumber: 5,
+//   },
+//   {
+//     artwork: '',
+//     audio: '',
+//     title: 'Cascadia',
+//     trackNumber: 6,
+//   },
+//   {
+//     artwork: '',
+//     audio: '',
+//     title: 'An Evening Trip',
+//     trackNumber: 7,
+//   },
+//   {
+//     artwork: '',
+//     audio: '',
+//     title: 'Ballad For a Figment',
+//     trackNumber: 8,
+//   },
+//   {
+//     artwork: '',
+//     audio: '',
+//     title: 'Rattle Me',
+//     trackNumber: 9,
+//   },
+//   {
+//     artwork: '',
+//     audio: '',
+//     title: 'A Melody of Old',
+//     trackNumber: 10,
+//   },
+// ];
+
+// export const faitAccompli = [
+//   {
+//     artwork: '',
+//     audio: '',
+//     title: 'Snooze',
+//     trackNumber: 1,
+//   },
+//   {
+//     artwork: '',
+//     audio: '',
+//     title: 'Write the Wrongs',
+//     trackNumber: 2,
+//   },
+//   {
+//     artwork: '',
+//     audio: '',
+//     title: 'A Quiet Cult',
+//     trackNumber: 3,
+//   },
+//   {
+//     artwork: '',
+//     audio: '',
+//     title: 'Strayaway',
+//     trackNumber: 4,
+//   },
+//   {
+//     artwork: '',
+//     audio: '',
+//     title: 'Sloth & Decay',
+//     trackNumber: 5,
+//   },
+//   {
+//     artwork: '',
+//     audio: '',
+//     title: 'Chimera',
+//     trackNumber: 6,
+//   },
+//   {
+//     artwork: '',
+//     audio: '',
+//     title: 'Pitter Patter',
+//     trackNumber: 7,
+//   },
+//   {
+//     artwork: '',
+//     audio: '',
+//     title: 'Go Alight',
+//     trackNumber: 8,
+//   },
+//   {
+//     artwork: '',
+//     audio: '',
+//     title: 'Warhorse',
+//     trackNumber: 9,
+//   },
+//   {
+//     artwork: '',
+//     audio: '',
+//     title: 'Big as Life',
+//     trackNumber: 10,
+//   },
+//   {
+//     artwork: '',
+//     audio: '',
+//     title: 'String Me Up',
+//     trackNumber: 11,
+//   },
+//   {
+//     artwork: '',
+//     audio: '',
+//     title: 'Out in Rays',
+//     trackNumber: 12,
+//   },
+//   {
+//     artwork: '',
+//     audio: '',
+//     title: 'Systems, Mobs, and Bob',
+//     trackNumber: 13,
+//   },
+// ];
+
+export const simulacra = [
+  {
+    artwork: `${ARTWORK}/simulacra-cover.jpg`,
+    audio: `${AUDIO}/simulacra/quagmire.mp3`,
+    title: 'Quagmire',
+    trackNumber: 1,
+  },
+  {
+    artwork: `${ARTWORK}/simulacra-cover.jpg`,
+    audio: `${AUDIO}/simulacra/tabs-on-your-insides.mp3`,
+    title: 'Tabs On Your Insides',
+    trackNumber: 2,
+  },
+  {
+    artwork: `${ARTWORK}/simulacra-cover.jpg`,
+    audio: `${AUDIO}/simulacra/in-your-crime.mp3`,
+    title: 'In Your Crime',
+    trackNumber: 3,
+  },
+  {
+    artwork: `${ARTWORK}/simulacra-cover.jpg`,
+    audio: `${AUDIO}/simulacra/mammoth-and-the-rage.mp3`,
+    title: 'Mammoth & the Rage',
+    trackNumber: 4,
+  },
+  {
+    artwork: `${ARTWORK}/simulacra-cover.jpg`,
+    audio: `${AUDIO}/simulacra/thumper.mp3`,
+    title: 'Thumper',
+    trackNumber: 5,
+  },
+  {
+    artwork: `${ARTWORK}/simulacra-cover.jpg`,
+    audio: `${AUDIO}/simulacra/nova-lady-ache.mp3`,
+    title: 'Nova Lady Ache',
+    trackNumber: 6,
+  },
+  {
+    artwork: `${ARTWORK}/simulacra-cover.jpg`,
+    audio: `${AUDIO}/simulacra/and-come-undone.mp3`,
+    title: 'And Come Undone',
+    trackNumber: 7,
+  },
+  {
+    artwork: `${ARTWORK}/simulacra-cover.jpg`,
+    audio: `${AUDIO}/simulacra/trepidation-highway.mp3`,
+    title: 'Trepidation Highway',
+    trackNumber: 8,
+  },
+  {
+    artwork: `${ARTWORK}/simulacra-cover.jpg`,
+    audio: `${AUDIO}/simulacra/simulacrum-i.mp3`,
+    title: 'Simulacrum I',
+    trackNumber: 9,
+  },
+  {
+    artwork: `${ARTWORK}/simulacra-cover.jpg`,
+    audio: `${AUDIO}/simulacra/southsea-hants.mp3`,
+    title: 'Southsea Hants',
+    trackNumber: 10,
+  },
+  {
+    artwork: `${ARTWORK}/simulacra-cover.jpg`,
+    audio: `${AUDIO}/simulacra/wear-their-hunger-and-their-haunt.mp3`,
+    title: 'Wear Their Hunger & Their haunt',
+    trackNumber: 11,
+  },
+];
