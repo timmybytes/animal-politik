6e9915f5b184b19c1c33435eea4dfc5651613289
Timothy Merritt <timmybytes@gmail.com>
2021-07-05 17:36:41 -0700

    chore(Store): Add prodcut preview modals

--- pages/store.js
+++ pages/store.js
@@ -3,23 +3,29 @@ import styles from '@styles/Store.module.scss';
 import React, { useState } from 'react';
 import products from '../store/products.json';
 
-const Product = ({ title, id, description, price, images, tags }) => {
-  const [showInfo, setShowInfo] = useState(false);
+const ProductPreview = ({
+  onClick,
+  title,
+  id,
+  description,
+  price,
+  images = [],
+  tags = [],
+}) => {
+  const handleClick = e => {
+    onClick();
+  };
   return (
-    <div key={id} className={styles.storeGrid__item}>
-      <img src={images[0]} alt='placeholder' />
-      {!showInfo ? (
-        <Button type='flat' onClick={() => setShowInfo(!showInfo)}>
-          Info
-        </Button>
-      ) : (
-        <Button type='outline' onClick={() => setShowInfo(!showInfo)}>
-          X
-        </Button>
-      )}
-      <div
-        style={showInfo ? { display: 'flex' } : { display: 'none' }}
-        className={styles.storeGrid__itemInfo}>
+    <div className={styles.productPreview} key={id || '1'}>
+      {/* Column One */}
+      <div className={styles.productPreview__columnOne}>
+        {images.map((img, idx) => (
+          <img key={idx} src={img} alt={title || 'product image'} />
+        ))}
+      </div>
+      {/* Column two */}
+      <div className={styles.productPreview__columnTwo}>
+        <Button onClick={handleClick}>X</Button>
         <h3>{title || 'Product Title'}</h3>
         <p>{description || 'Product description'}</p>
         <p>${price || 'N/A'}</p>
@@ -30,22 +36,75 @@ const Product = ({ title, id, description, price, images, tags }) => {
   );
 };
 
+const Product = ({ onClick, title, id, description, price, images, tags }) => {
+  const handleClick = e => {
+    onClick(e.currentTarget.value);
+  };
+  return (
+    <div key={id} className={styles.storeGrid__item}>
+      <img src={images[0]} alt='placeholder' />
+      <div className={styles.storeGrid__itemInfo}>
+        <h3>{title || 'Product Title'}</h3>
+        <p>{description || 'Product description'}</p>
+        <p>${price || 'N/A'}</p>
+        <Button type='primary' value={id} onClick={e => handleClick(e)}>
+          Add to Cart
+        </Button>
+      </div>
+    </div>
+  );
+};
+
 const Store = ({ children }) => {
+  const [showPreview, setShowPreview] = useState({
+    visible: false,
+    product: '',
+  });
+  const handleClick = e => {
+    if (e) {
+      setShowPreview({ visible: true, product: e });
+    } else {
+      setShowPreview({ visible: false, product: '' });
+    }
+  };
   return (
     <div className={styles.page}>
       <h1 className='page_title'>Store</h1>
       <h2>Feed our capitalist hunger!</h2>
       <div className={styles.storeGrid}>
-        {products.map(product => (
+        {products.map(({ id, title, description, images, price, tags }) => (
           <Product
-            title={product.title}
-            description={product.description}
-            images={product.images}
-            price={product.price}
-            tags={product.tags}
+            key={id}
+            onClick={handleClick}
+            id={id}
+            title={title}
+            description={description}
+            images={images}
+            price={price}
+            tags={tags}
           />
         ))}
       </div>
+      {showPreview.visible && (
+        <div>
+          {products
+            .filter(product => product.id === showPreview.product)
+            .map(({ id, title, description, images, price, tags }) => {
+              console.log(id, title, description, images, price, tags);
+              return (
+                <ProductPreview
+                  onClick={handleClick}
+                  id={id}
+                  title={title}
+                  description={description}
+                  images={images}
+                  price={price}
+                  tags={tags}
+                />
+              );
+            })}
+        </div>
+      )}
     </div>
   );
 };
